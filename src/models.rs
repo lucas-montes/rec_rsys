a7e2eb5bdab6607bafdf68cf2df73dd0383efa77
Lucas <lluc23@hotmail.com>
2023-06-06 00:02:03 +0200

    trati to item

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,16 @@
 //! Place to store all the models used to calculate
 
+/// Generic model to save the results
+// Similarity struct: used to store the result of the similarities calculation
+// struct Result {
+//      id: u32,
+//      result: f32,
+//      confidence: f32,
+//      explanation: String,
+//      date: String / DateTime,
+//      algorithm: String,
+// }
+
 /// Generic model to perform calculations
 #[derive(Debug, Clone)]
 pub struct Item {
@@ -8,21 +19,21 @@ pub struct Item {
     /// Vector of values used to calculate
     pub values: Vec<f32>,
     /// The result of the similarity calculated
+    /// This could be changed into a new struct with more info
     pub result: f32,
+    // pub result: Result,
 }
 
 impl Item {
     /// New
-    pub fn new(id: u32, values: Vec<f32>, result: f32) -> Self {
-        Item { id, values, result }
+    pub fn new(id: u32, values: Vec<f32>, result: Option<f32>) -> Self {
+        Item {
+            id,
+            values,
+            result: result.unwrap_or(f32::NAN),
+        }
     }
-    // pub fn new(id: u32, values: Vec<f32>) -> Self {
-    //     Item {
-    //         id,
-    //         values,
-    //         result: f32::NAN,
-    //     }
-    // }
+
     pub fn result(mut self, r: f32) -> Self {
         self.result = r;
         self
@@ -45,4 +56,8 @@ impl<'a> PartialEq<&'a Item> for Item {
     fn eq(&self, other: &&'a Item) -> bool {
         self == *other
     }
+}
+
+pub trait ItemAdapter {
+    fn to_item(&self) -> Item;
 }
